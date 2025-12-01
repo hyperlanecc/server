08f6c6a480ccc9fc9fe1cbadee73114366863098
yaco <yaco@yaco.email>
2025-12-01 21:42:05 +0800

    feat: restructure API routes for better organization

--- routes/routes.go
+++ routes/routes.go
@@ -10,61 +10,64 @@ import (
 func SetupRouter(r *gin.Engine) {
 	r.Use(middlewares.Cors())
 
-	r.GET("/v1/auth/callback", controllers.HandleOAuthCallback)
+	api := r.Group("/api")
+	{
+		api.GET("/v1/auth/callback", controllers.HandleOAuthCallback)
 
-	r.POST("/v1/login", controllers.HandleLogin)
+		api.POST("/v1/login", controllers.HandleLogin)
 
-	user := r.Group("v1/users")
-	{
-		user.PUT("/:id", middlewares.JWT(""), controllers.UpdateUser)
-		user.GET("/:id", controllers.GetUser)
-		user.POST("/follow/:id", middlewares.JWT(""), controllers.FollowUser)
-		user.POST("/unfollow/:id", middlewares.JWT(""), controllers.UnfollowUser)
-		user.POST("/follow/states", middlewares.JWT(""), controllers.GetFollowStates)
-	}
+		user := api.Group("v1/users")
+		{
+			user.PUT("/:id", middlewares.JWT(""), controllers.UpdateUser)
+			user.GET("/:id", controllers.GetUser)
+			user.POST("/follow/:id", middlewares.JWT(""), controllers.FollowUser)
+			user.POST("/unfollow/:id", middlewares.JWT(""), controllers.UnfollowUser)
+			user.POST("/follow/states", middlewares.JWT(""), controllers.GetFollowStates)
+		}
 
-	event := r.Group("/v1/events")
-	{
-		event.POST("", middlewares.JWT("event:write"), controllers.CreateEvent)
-		event.DELETE("/:id", middlewares.JWT("event:delete"), controllers.DeleteEvent)
-		event.PUT("/:id", middlewares.JWT("event:write"), controllers.UpdateEvent)
-		event.GET("", controllers.QueryEvents)
-		event.GET("/:id", controllers.GetEvent)
-		event.PUT("/:id/status", middlewares.JWT("event:review"), controllers.UpdateEventPublishStatus)
+		event := api.Group("/v1/events")
+		{
+			event.POST("", middlewares.JWT("event:write"), controllers.CreateEvent)
+			event.DELETE("/:id", middlewares.JWT("event:delete"), controllers.DeleteEvent)
+			event.PUT("/:id", middlewares.JWT("event:write"), controllers.UpdateEvent)
+			event.GET("", controllers.QueryEvents)
+			event.GET("/:id", controllers.GetEvent)
+			event.PUT("/:id/status", middlewares.JWT("event:review"), controllers.UpdateEventPublishStatus)
 
-		// 发布博客是用户默认权限， 这里任何用户都可以添加recap
-		event.POST("/recap", middlewares.JWT("blog:write"), controllers.CreateReacp)
-		event.DELETE("/recap/:id", middlewares.JWT("blog:delete"), controllers.DeleteRecap)
-		event.PUT("/recap/:id", middlewares.JWT("blog:write"), controllers.UpdateRecap)
-		event.GET("/recap", controllers.GetRecap)
+			// 发布博客是用户默认权限， 这里任何用户都可以添加recap
+			event.POST("/recap", middlewares.JWT("blog:write"), controllers.CreateReacp)
+			event.DELETE("/recap/:id", middlewares.JWT("blog:delete"), controllers.DeleteRecap)
+			event.PUT("/recap/:id", middlewares.JWT("blog:write"), controllers.UpdateRecap)
+			event.GET("/recap", controllers.GetRecap)
+		}
+		blog := api.Group("/v1/blogs")
+		{
+			blog.POST("", middlewares.JWT("blog:write"), controllers.CreateArticle)
+			blog.DELETE("/:id", middlewares.JWT("blog:delete"), controllers.DeleteArticle)
+			blog.PUT("/:id", middlewares.JWT("blog:write"), controllers.UpdateArticle)
+			blog.GET("/:id", controllers.GetArticle)
+			blog.GET("", controllers.QueryArticles)
+			blog.PUT("/:id/status", middlewares.JWT("blog:review"), controllers.UpdateArticlePublishStatus)
+		}
+		feedback := api.Group("/v1/feedbacks")
+		{
+			feedback.POST("", middlewares.JWT(""), controllers.CreateFeedback)
+			feedback.GET("", controllers.QueryFeedbacks)
+		}
+		post := api.Group("/v1/posts")
+		{
+			post.POST("", middlewares.JWT("blog:write"), controllers.CreatePost)
+			post.DELETE("/:id", middlewares.JWT("blog:delete"), controllers.DeletePost)
+			post.GET("/:id", controllers.GetPost)
+			post.PUT("/:id", middlewares.JWT("blog:write"), controllers.UpdatePost)
+			post.GET("", controllers.QueryPosts)
+			post.GET("/stats", controllers.PostsStats)
+			post.POST("/:id/like", middlewares.JWT(""), controllers.LikePost)
+			post.POST("/:id/unlike", middlewares.JWT(""), controllers.UnlikePost)
+			post.POST("/:id/favorite", middlewares.JWT(""), controllers.FavoritePost)
+			post.POST("/:id/unfavorite", middlewares.JWT(""), controllers.UnfavoritePost)
+			post.GET("/status", middlewares.JWT(""), controllers.GetPostStatus)
+		}
+		api.GET("/v1/stats", controllers.StatsOverview)
 	}
-	blog := r.Group("/v1/blogs")
-	{
-		blog.POST("", middlewares.JWT("blog:write"), controllers.CreateArticle)
-		blog.DELETE("/:id", middlewares.JWT("blog:delete"), controllers.DeleteArticle)
-		blog.PUT("/:id", middlewares.JWT("blog:write"), controllers.UpdateArticle)
-		blog.GET("/:id", controllers.GetArticle)
-		blog.GET("", controllers.QueryArticles)
-		blog.PUT("/:id/status", middlewares.JWT("blog:review"), controllers.UpdateArticlePublishStatus)
-	}
-	feedback := r.Group("/v1/feedbacks")
-	{
-		feedback.POST("", middlewares.JWT(""), controllers.CreateFeedback)
-		feedback.GET("", controllers.QueryFeedbacks)
-	}
-	post := r.Group("/v1/posts")
-	{
-		post.POST("", middlewares.JWT("blog:write"), controllers.CreatePost)
-		post.DELETE("/:id", middlewares.JWT("blog:delete"), controllers.DeletePost)
-		post.GET("/:id", controllers.GetPost)
-		post.PUT("/:id", middlewares.JWT("blog:write"), controllers.UpdatePost)
-		post.GET("", controllers.QueryPosts)
-		post.GET("/stats", controllers.PostsStats)
-		post.POST("/:id/like", middlewares.JWT(""), controllers.LikePost)
-		post.POST("/:id/unlike", middlewares.JWT(""), controllers.UnlikePost)
-		post.POST("/:id/favorite", middlewares.JWT(""), controllers.FavoritePost)
-		post.POST("/:id/unfavorite", middlewares.JWT(""), controllers.UnfavoritePost)
-		post.GET("/status", middlewares.JWT(""), controllers.GetPostStatus)
-	}
-	r.GET("/v1/stats", controllers.StatsOverview)
 }
